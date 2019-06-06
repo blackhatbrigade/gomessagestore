637883503d9c979432bb8765fcf60870f278828c
Nathan Bell <nathan.bell@berkadia.com>
2019-06-06 14:17:07 -0600

    adds a checkWriteOptions function and options interface

--- messagestore.go
+++ messagestore.go
@@ -12,8 +12,8 @@ import (
 
 //MessageStore Establishes the interface for Eventide.
 type MessageStore interface {
-	Write(ctx context.Context, message Message, opts ...WriteOptions) error
-	Get(ctx context.Context, opts ...GetOptions) ([]Message, error)
+	Write(ctx context.Context, message Message, opts ...WriteOption) error
+	Get(ctx context.Context, opts ...GetOption) ([]Message, error)
 	//WriteWithExpectedPosition(ctx context.Context, message *Message, version int64) error
 }
 
@@ -21,11 +21,17 @@ type msgStore struct {
 	repo repository.Repository
 }
 
-//WriteOptions provide optional arguments to the Write function
-type WriteOptions func(ms *msgStore)
+type writer struct {
+	atPosition *int64
+}
 
-//GetOptions provide optional arguments to the Get function
-type GetOptions func(ms *msgStore)
+type getter struct{}
+
+//WriteOption provide optional arguments to the Write function
+type WriteOption func(w *writer)
+
+//GetOption provide optional arguments to the Get function
+type GetOption func(g *getter)
 
 //GetMessageStoreInterface Grabs a MessageStore instance.
 func GetMessageStoreInterface(injectedDB *sql.DB) MessageStore {
@@ -47,8 +53,16 @@ func GetMessageStoreInterface2(injectedRepo repository.Repository) MessageStore 
 	return msgstr
 }
 
+func checkWriteOptions(opts ...WriteOption) *writer {
+	w := &writer{}
+	for _, option := range opts {
+		option(w)
+	}
+	return w
+}
+
 //Write Writes a Message to the message store.
-func (ms *msgStore) Write(ctx context.Context, message Message, opts ...WriteOptions) error {
+func (ms *msgStore) Write(ctx context.Context, message Message, opts ...WriteOption) error {
 	envelope, err := message.ToEnvelope()
 	if err != nil {
 		logrus.WithError(err).Error("Write: Validation Error")
@@ -56,17 +70,28 @@ func (ms *msgStore) Write(ctx context.Context, message Message, opts ...WriteOpt
 		return err
 	}
 
-	err = ms.repo.WriteMessage(ctx, envelope)
+	writeOptions := checkWriteOptions(opts...)
+	if writeOptions.atPosition != nil {
+		err = ms.repo.WriteMessageWithExpectedPosition(ctx, envelope, *writeOptions.atPosition)
+	} else {
+		err = ms.repo.WriteMessage(ctx, envelope)
+	}
 	if err != nil {
 		logrus.WithError(err).Error("Write: Error writing message")
 
 		return err
 	}
-
 	return nil
 }
 
 //Get Gets one or more Messages from the message store.
-func (ms *msgStore) Get(ctx context.Context, opts ...GetOptions) ([]Message, error) {
+func (ms *msgStore) Get(ctx context.Context, opts ...GetOption) ([]Message, error) {
 	return nil, nil
+}
+
+//AtPosition allows for writing messages using an expected position
+func AtPosition(position int64) WriteOption {
+	return func(w *writer) {
+		w.atPosition = &position
+	}
 }
