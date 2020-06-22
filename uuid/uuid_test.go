a78deede771cc6f77c41f08510464f6c0d06b834
Nathan Bell <nathan.bell@berkadia.com>
2020-06-22 13:39:48 -0600

    adds the ability to convert the UUID to a string using capital letters

--- uuid/uuid_test.go
+++ uuid/uuid_test.go
@@ -6,6 +6,42 @@ import (
 	. "github.com/blackhatbrigade/gomessagestore/uuid"
 )
 
+func TestCanMarshallAllCaps(t *testing.T) {
+	uuid, _ := Parse("5fe1e4d9-d788-4bec-9b01-8be766127727")
+
+	marshalledLowercase, err := uuid.MarshalJSON()
+	if err != nil {
+		t.Errorf("An error occurred %+v", err)
+	}
+	if string(marshalledLowercase) != `"5fe1e4d9-d788-4bec-9b01-8be766127727"` {
+		t.Error("Marshall capitalized the letters when it wasn't asked to")
+	}
+
+	uuid.AllCaps = true
+	marshalledUppercase, err := uuid.MarshalJSON()
+	if err != nil {
+		t.Errorf("An error occurred %+v", err)
+	}
+	if string(marshalledUppercase) != `"5FE1E4D9-D788-4BEC-9B01-8BE766127727"` {
+		t.Error("Marshall did not capitalize the letters")
+	}
+}
+
+func TestCanStringAllCaps(t *testing.T) {
+	uuid, _ := Parse("5fe1e4d9-d788-4bec-9b01-8be766127727")
+
+	stringedLowercase := uuid.String()
+	if stringedLowercase != `5fe1e4d9-d788-4bec-9b01-8be766127727` {
+		t.Error("String capitalized the letters when it wasn't asked to")
+	}
+
+	uuid.AllCaps = true
+	stringedUppercase := uuid.String()
+	if stringedUppercase != `5FE1E4D9-D788-4BEC-9B01-8BE766127727` {
+		t.Error("String did not capitalize the letters")
+	}
+}
+
 func TestCanMarshallAndUnMarshall(t *testing.T) {
 	uuid := NewRandom()
 	marshalled, err := uuid.MarshalJSON()
